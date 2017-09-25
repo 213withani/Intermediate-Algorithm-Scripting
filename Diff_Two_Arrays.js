f8c5b3edcde9ca4a8cb01a21258a31bbda034833
Israel <213withani@gmail.com>
2017-09-24 20:50:48 -0700

    Compare an array content another individual array content.

---
+++ Diff_Two_Arrays.js
@@ -0,0 +1,10 @@
+
+function diffArray(arr1, arr2) {
+    var newArr = [];
+    // Same, same; but different.
+    return arr1.indexOf(arr2[1]);
+    
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+  
