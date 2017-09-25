286e69fd5d6ad9a5f92637c0c58fa036ce8bd100
Israel <213withani@gmail.com>
2017-09-24 22:41:12 -0700

    refactor

--- Diff_Two_Arrays.js
+++ Diff_Two_Arrays.js
@@ -2,28 +2,18 @@
 function diffArray(arr1, arr2) {
     var newArr = [];
     var newArr2 = [];
-    var smallArr = [];
-    var bigArr = [];
 
-    if (arr1.length > arr2.length) {
-        bigArr = arr1;
-        smallArr = arr2;
-    } else {
-        smallArr = arr1;
-        bigArr = arr2;
-    }
-
-    newArr = bigArr.filter(function (param) {
+    firstUniqueElement = arr1.filter(function (param) {
         // Check each bigArr element against the small array.
-        return smallArr.indexOf(param) < 0;
+        return arr2.indexOf(param) < 0;
     });
 
-    newArr2 = smallArr.filter(function (param) {
+    UniqueElement = arr2.filter(function (param) {
         // Check each bigArr element against the small array.
-        return bigArr.indexOf(param) < 0;
+        return arr1.indexOf(param) < 0;
     });
 
-    return newArr.concat(newArr2);
+    return firstUniqueElement.concat(UniqueElement);
 }
 
 diffArray([1, 2, 3, 4], [1, 2, 3, 5]);
